7f97be506b87218f231f81d403a96c8a9576f4e4
michellecaner <michellecaner@gmail.com>
2022-01-31 20:11:31 -0600

    got the basics rendering

---
+++ src/components/Kennel.js
@@ -0,0 +1,23 @@
+import React from "react"
+// does "./animal/AnimalCard" have to be "./animal/AnimalCard.js??"
+import { AnimalCard } from "./animal/AnimalCard"
+import "./Kennel.css"
+
+export const Kennel = () => (
+    <>
+        <h2>Nashville Kennels</h2>
+        <small>Loving care when you're not there.</small>
+
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </article>
+    </>
+)
