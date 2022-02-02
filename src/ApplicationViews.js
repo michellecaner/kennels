100ed1ebd693e45fd0adfde2e38e7a21b420dc91
michellecaner <michellecaner@gmail.com>
2022-02-01 20:03:47 -0600

    completed chapter 7 & added additional nav bar routes

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./Home"
-import { AnimalCard } from './components/animal/AnimalCard.js'
+import { AnimalCard } from "./components/animal/AnimalCard"
+import { LocationCard } from "./components/location/LocationCard"
+import { CustomerCard } from './components/customer/CustomerCard'
+import { EmployeeCard } from './components/employee/EmployeeCard'
 
 
 export const ApplicationViews = () => {
@@ -10,9 +13,11 @@ export const ApplicationViews = () => {
             <Routes>
                 {/* Render the location list when http://localhost:3000/ */}
                 <Route exact path="/" element={<Home />} />
-
+                <Route path="/locations" element={<LocationCard />} />
                 {/* Render the animal list when http://localhost:3000/animals */}
                 <Route path="/animals" element={<AnimalCard />} />
+                <Route path="/customers" element={<CustomerCard />} />
+                <Route path="/employees" element={<EmployeeCard />} />
             </Routes>
         </>
     )
