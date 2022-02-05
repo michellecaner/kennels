7db4114506200488bcdb67714b7e50943c43814f
michellecaner <michellecaner@gmail.com>
2022-02-05 10:30:48 -0600

    created module & list component for Customers and displayed a static designed card

---
+++ src/components/customer/CustomerList.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { CustomerCard } from './CustomerCard';
+import { getAllCustomers, getCustomerById } from '../../modules/CustomerManager';
+
+export const CustomerList = () => {
+  // The initial state is an empty array
+  const [customers, setCustomers] = useState([]);
+
+  const getCustomers = () => {
+    // After the data comes back from the API, we
+    //  use the setCustomers function to update state
+    return getAllCustomers().then(customersFromAPI => {
+      setCustomers(customersFromAPI)
+    });
+  };
+
+  // got the animals from the API on the component's first render
+  useEffect(() => {
+    getCustomers();
+  }, []);
+
+  // Finally we use .map() to "loop over" the animals array to show a list of animal cards
+  return (
+    <div className="container-cards">
+      {customers.map(customer => <CustomerCard key={customer.id} />)}
+    </div>
+  );
+};
