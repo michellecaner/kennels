ae16804f90c21a4006d2dd9c528a2ddccb4ddf2b
michellecaner <michellecaner@gmail.com>
2022-02-08 19:41:57 -0600

    added close location button to location details

--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { getLocationById } from '../../modules/LocationManager';
+import { deleteLocation, getLocationById } from '../../modules/LocationManager';
 import './LocationDetail.css';
 import { useParams, useNavigate } from "react-router-dom"
 
 export const LocationDetail = () => {
   const [location, setLocation] = useState({ name: "", address: "" });
+  const [isLoading, setIsLoading] = useState(true);
+
 
   const {locationId} = useParams();
   const navigate = useNavigate();
@@ -15,13 +17,25 @@ export const LocationDetail = () => {
     getLocationById(locationId)
       .then(location => {
         setLocation(location);
+        setIsLoading(false);
       });
   }, [locationId]);
+
+  const handleDelete = () => {
+    //invoke the delete function in LocationManger and re-direct to the location list.
+    setIsLoading(true);
+    deleteLocation(locationId).then(() =>
+      navigate("/locations")
+    );
+  };
 
   return (
     <section className="location">
       <h3 className="location__name">{location.name}</h3>
       <div className="address">{location.address}</div>
+      <button type="button" disabled={isLoading} onClick={handleDelete}>
+          Close Location
+        </button>
     </section>
   );
 }
